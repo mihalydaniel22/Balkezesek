da86fcf609fe4a695bbd4e010cff7e7b26d1dcb1
mihalydaniel22 <rikkiardo3@gmail.com>
2020-10-16 11:35:29 +0200

    Add project files.

---
+++ Balkezesek/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Balkezesek
+{
+    class Program
+    {
+        static List<Versenyzok> adatok = new List<Versenyzok>();
+
+        static void MasodikFeladat()
+        {
+            StreamReader file = new StreamReader("balkezesek.csv");
+
+            file.ReadLine();
+
+            while (!file.EndOfStream)
+            {
+                string[] adat = file.ReadLine().Split(';');
+                adatok.Add(new Versenyzok(adat[0], adat[1], adat[2], int.Parse(adat[3]), int.Parse(adat[4])));
+            }
+            file.Close();
+        }
+        static void HarmadikFeladat()
+        {
+            int sorok = 0;
+            foreach (var a in adatok)
+            {
+                sorok++;
+            }
+            Console.WriteLine("3. feladat : {0}", sorok);
+        }
+        static void NegyedikFeladat()
+        {
+            
+        }
+
+        static void Main(string[] args)
+        {
+            MasodikFeladat();
+            HarmadikFeladat();
+            NegyedikFeladat();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Balkezesek/Versenyzok.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Balkezesek
+{
+    class Versenyzok
+    {
+        public string Nev { get; private set; }
+        public string Elso { get; private set; }
+        public string Utolso { get; private set; }
+        public int Suly { get; private set; }
+        public int Magassag { get; private set; }
+
+        public Versenyzok(string nev, string elso, string utolso, int suly, int magassag)
+        {
+            this.Nev = nev;
+            this.Elso = elso;
+            this.Utolso = utolso;
+            this.Suly = suly;
+            this.Magassag = magassag;
+        }
+    }
+}
